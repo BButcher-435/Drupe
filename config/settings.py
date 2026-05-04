2e5d703431c187cf5f9dc6ea87f3d19dcea59ded
boratasi <boraeminsoyalp@hotmail.com>
2026-05-05 00:37:49 +0300

    Canlı ses analizi (librosa) ve Spotify görsel arayüzü başarıyla entegre edildi

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+
+# --- EQUALIZER APO AYARLARI ---
+# Equalizer APO'nun kurulu olduğu yerdeki config dosyası
+APO_CONFIG_PATH = r"C:\Program Files\EqualizerAPO\config\config.txt"
+
+# --- SPOTIFY API AYARLARI ---
+# API'ye çok sık istek atıp banlanmamak için bekleme süresi (saniye)
+API_FETCH_INTERVAL = 5
